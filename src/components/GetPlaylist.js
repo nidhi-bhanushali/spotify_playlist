076ca1556ff168d66b88780e129d29b71f399bf3
Nidhi Bhanushali <bhanushalinidhi44@gmail.com>
2021-04-08 20:50:52 +0530

    Initial setup

---
+++ src/components/GetPlaylist.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const GetPlaylist = () => {
+
+    const [playlist , setPlaylist] = useState([])
+
+    return ( 
+        <div>
+            
+        </div>
+    )
+}
+
+export default GetPlaylist
