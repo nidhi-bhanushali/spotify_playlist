46231bc1cf277d3aaa269da5b01303b2600324e2
Nidhi Bhanushali <bhanushalinidhi44@gmail.com>
2021-04-09 02:08:19 +0530

    Added button for handling clicks

--- src/components/GetPlaylist.js
+++ src/components/GetPlaylist.js
@@ -28,22 +28,32 @@ const GetPlaylist = () => {
     .then(({data}) => setPlaylist(data.playlists.items))
     } );
 
-    
     }, [])
+
+    const handleClick = () => {
+        console.log('Added to localStorage')  
+    }
 
     return ( 
         <div>
             {playlist && playlist.map((playlist) => ( 
              <div key = {playlist.id} className = 'card m-5'>   
-            <h2 
-            className='card-title text-center mt-4'
-            style={{color:'#1db954'}}
-            >{playlist.name}</h2>
+                <h2 
+                className='card-title text-center mt-4'
+                style={{color:'#1db954'}}>
+                        {playlist.name}
+                </h2>
                 <img className='card-body' 
                 src = {playlist.images[0].url} 
                 alt= 'playlist'
                 style={{width:300 , height: 300}}
                 ></img>
+                <button style = {{background : '#1db954'}} 
+                className='btn btn-block'
+                onClick = {handleClick}
+                >
+                Add to Local</button>
+
             </div>
             ))}
         </div>
